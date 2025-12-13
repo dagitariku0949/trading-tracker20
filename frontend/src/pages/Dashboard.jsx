36418737b70bdf18d85353bc6449c472a59ce792
dagitariku0949 <dagitariku0949@users.noreply.github.com>
2025-12-13 23:37:40 +0300

    Delete ALL auth-related files completely

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react'
-import api from '../api/client'
 import ConfluenceCard from '../components/ConfluenceCard'
 import CalendarHeatmap from '../components/CalendarHeatmap'
 import MetricsGrid from '../components/MetricsGrid'
